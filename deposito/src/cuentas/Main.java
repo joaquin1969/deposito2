138fd96cf15a64378321cca0887624f7d5f9462c
joaquin1969 <joaquinmoll@gmail.com>
2023-03-07 15:14:06 +0100

    Refactorización - Punto 5 - nuevo parámetro operativa_cuenta
    Añadir un nuevo parámetro al método operativa_cuenta, de nombre cantidad
    y de tipo float. Esa cantidad será la utilizada en la creación de la
    cuenta.
    cambiar el nomrbre al método extracted()

--- deposito/src/cuentas/Main.java
+++ deposito/src/cuentas/Main.java
@@ -1,15 +1,20 @@
 package cuentas;
 public class Main {
 
-    public static void main(String[] args) {
-        extracted();
+    private static float cantidad;
+   
+    
+	public static void main(String[] args) {
+		 cantidad = 2500;
+        operativa_cuenta();
     }
 
-	private static void extracted() {
+	private static void operativa_cuenta() {
 		CCuenta cuenta1;
         double saldoActual;
-
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        
+        
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",cantidad,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
