342529ba3f367e7c825b7eb805f493fb13d00f9d
joaquin1969 <joaquinmoll@gmail.com>
2023-03-09 17:41:50 +0100

    Javadoc - Generar documentación de la clase CCuenta

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -1,28 +1,52 @@
 package cuentas;
 public class CCuenta {
-
+	/**
+	 * Clase CCuenta como la del banco y permite hacer ingresos y retiradas de dinero
+	 * 
+	 * @author IES Avellaneda
+	 * @version 1 
+	 */
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Constructor vacio, crea una cuenta vacia
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Contructor
+     * @param nom Nombre de la cuenta
+     * @param cue Número de la cuenta
+     * @param sal Saldo
+     * @param tipo Tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
     }
-
+    
+    /**
+     * Getter del saldo de la cuenta
+     * @return Saldo
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Método para hacer un ingreso
+     * @param cantidad a ingresar
+     * @throws Exception No se puede ingresar cantidades negativas
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -30,6 +54,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Método para hacer una retirada
+     * @param cantidad a retirar
+     * @throws Exception No se pueden retirar cantidades negativas
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,34 +68,66 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+    /**
+     * Getter del nombre de la cuenta
+     * @return nombre
+     */
 	private String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Setter del nombre de la cuenta
+	 * @param nombre
+	 */
 	private void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Getter del número de la cuenta
+	 * @return cuenta
+	 */
 	private String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * Setter del número de la cuenta
+	 * @param cuenta
+	 */
 	private void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * Getter del saldo
+	 * @return saldo
+	 */
 	private double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * Setter del saldo
+	 * @param saldo
+	 */
 	private void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * Getter del tipo de interés
+	 * @return tipoInterés
+	 */
 	private double getTipoInterés() {
 		return tipoInterés;
 	}
 
+	/**
+	 * Setter del tipo de interés
+	 * @param tipoInterés
+	 */
 	private void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
